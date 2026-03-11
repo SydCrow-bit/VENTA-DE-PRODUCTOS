38f5396cb2d4fde1f60cff00427c43ee1381c1c1
Fernando Nina <Fernandonina611@gmail.com>
2026-03-10 21:30:41 -0400

    feat: :sparkles: agreegando manejo de variables de entorno para uso de chatbot y configuracion de la DB

--- config.py
+++ config.py
@@ -1,6 +1,16 @@
-class Config:
-    SECRET_KEY = "supersecretkey"
+import os
+from dotenv import load_dotenv
 
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost:3306/db_venta_electronicos"
+# Carga las variables del archivo .env al entorno de Python
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "supersecretkey")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL", 
+        "mysql+pymysql://root@localhost:3306/db_venta_electronicos"
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
